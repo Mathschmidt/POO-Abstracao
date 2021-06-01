256fa4960ca663a83d358445846ebb8b6882d398
NayaraPero <71534449+NayaraPero@users.noreply.github.com>
2021-05-31 23:23:39 -0300

    Update Ambiente.java

--- SistemaAcademico/src/sistemaacademico/classes/Ambiente.java
+++ SistemaAcademico/src/sistemaacademico/classes/Ambiente.java
@@ -2,7 +2,33 @@ package sistemaacademico.classes;
 
 import java.text.MessageFormat;
 
+
 public class Ambiente {
     
-
+    private int Id;
+    private String Nome;
+    private int TipoAmbienteId;
+    private boolean Ativo; 
+    
+    // Construtor do Ambiente
+    public Ambiente(int id, String nome, int tipoambienteid, boolean ativo){
+        this.Id = id;
+        this.Nome = nome;
+        this.TipoAmbienteId = tipoambienteid;
+        this.Ativo = ativo;
+    }   
+    
+    // Desativa o Ambiente
+    public void Desativar(int id){
+        if(this.Id == id){
+            this.Ativo = false;
+        }else{
+            System.out.println("Não foi possível desativar o ambiente, Id incorreto!");
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return MessageFormat.format("Id: {0}, Nome: {1}, TipoAmbienteId: {2},  Ativo: {3}", Id, Nome, TipoAmbienteId, Ativo);
+    }
 }
